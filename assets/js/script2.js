6425efbdf175da69581cd25164e0b2f97d548b22
Alexanderalavi <alexanderalaviperez@gmail.com>
2025-04-10 23:33:11 -0400

    cambio color presionando asd

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -14,7 +14,9 @@ document.querySelector("#blue").addEventListener("click", function() {
     this.style.backgroundColor = "black"
   })
 
-
+//q = morado 
+//w = gris
+//e = cafe
   document.addEventListener('keydown', function (event) {
     const key = document.querySelector('#key')
     if (event.key === 'a') {
@@ -23,6 +25,12 @@ document.querySelector("#blue").addEventListener("click", function() {
     key.style.backgroundColor = 'orange'
     } else if (event.key === 'd'){
       key.style.backgroundColor = 'lightblue'
+    } else if (event.key === 'q' ) {
+      createNewDiv('purple')
+    } else if (event.key === 'w' ) {
+      createNewDiv('grey')
+    } else if (event.key === 'e' ) {
+      createNewDiv('brown')
     }
     })
     
