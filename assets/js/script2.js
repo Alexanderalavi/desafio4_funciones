789deb3f8e2fab3955ce419da1301ad32f1d800c
Alexanderalavi <alexanderalaviperez@gmail.com>
2025-04-10 23:16:41 -0400

    asd cambio de color agregado

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -13,3 +13,16 @@ document.querySelector("#blue").addEventListener("click", function() {
   document.querySelector("#yellow").addEventListener("click", function() {
     this.style.backgroundColor = "black"
   })
+
+
+  document.addEventListener('keydown', function (event) {
+    const key = document.querySelector('#key')
+    if (event.key === 'a') {
+    key.style.backgroundColor = 'pink'
+    } else if (event.key === 's') {
+    key.style.backgroundColor = 'orange'
+    } else if (event.key === 'd'){
+      key.style.backgroundColor = 'lightblue'
+    }
+    })
+    
